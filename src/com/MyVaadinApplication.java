085b81ed89f43d00344e750166041ada0c0e044a
Victory <git@dfhu.org>
2015-11-05 15:23:43 -0800

    add sqlite support as PoC

--- src/com/MyVaadinApplication.java
+++ src/com/MyVaadinApplication.java
@@ -5,6 +5,10 @@ import com.vaadin.data.util.sqlcontainer.connection.SimpleJDBCConnectionPool;
 import com.vaadin.server.Page;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.*;
+import org.eclipse.jetty.util.log.Log;
+
+import java.sql.SQLException;
+import java.util.logging.Logger;
 
 /**
  * comes
@@ -25,7 +29,33 @@ public class MyVaadinApplication extends UI {
         mainLayout.addComponent(textInputLayout);
 
         Button saveButton = new Button("Add Phrase");
-        saveButton.addClickListener(clickEvent -> Notification.show("Need to save"));
+        saveButton.addClickListener(new Button.ClickListener() {
+            @Override
+            public void buttonClick(Button.ClickEvent clickEvent) {
+                getDbPool();
+            }
+        });
         mainLayout.addComponent(saveButton);
+    }
+
+    private SimpleJDBCConnectionPool getDbPool () {
+        String path = System.getProperty("user.home") + "/db/russian.sqlite";
+        SimpleJDBCConnectionPool pool;
+        Logger.getLogger("testing").warning(path);
+        try {
+            pool = new SimpleJDBCConnectionPool(
+                    "org.sqlite.JDBC",
+                    "jdbc:sqlite:" + path, "", "");
+        } catch (SQLException e) {
+            Notification.show("sqlite error " + e.getMessage());
+            return null;
+        } catch (RuntimeException e) {
+            Notification.show("runtime sqlite error " + e.getMessage());
+            return null;
+        }
+
+        Notification.show("Got pool");
+        return pool;
+
     }
 }
